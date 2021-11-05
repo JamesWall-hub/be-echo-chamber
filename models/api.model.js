9846bfa8d9cb0b7d084522a0be726dc0a42ba300
JamesWall-hub <james.sean.wall@gmail.com>
2021-11-05 11:32:27 +0000

    updates model

--- models/api.model.js
+++ models/api.model.js
@@ -6,7 +6,11 @@ exports.readEndPoints = () => {
     return fs.readFile('./endpoints.json', 'utf8' , (err, data) => {
         if (err) {
           return err
+        } else {
+          return data
         }
-        return JSON.parse(data)
+    })
+    .then((data) => {
+      return JSON.parse(data)
     })
 } 
